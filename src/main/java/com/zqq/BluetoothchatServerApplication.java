f7fbfc0436bad7bebdd81792d1a02f8e0512b6e9
CORP\zhuqingquan <zhuqingquan@ppdai.com>
2017-03-21 12:55:25 +0800

    Initial commit

---
+++ src/main/java/com/zqq/BluetoothchatServerApplication.java
@@ -0,0 +1,12 @@
+package com.zqq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluetoothchatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluetoothchatServerApplication.class, args);
+	}
+}
